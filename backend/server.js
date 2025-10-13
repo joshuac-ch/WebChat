30a9b16b5516e3e958e03852e91974b7aabe74c8
joshua <condorenajoshua98@gmail.com>
2025-10-12 20:38:16 -0500

    Se modifico el permiso de cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import express from "express"
 import logger from "morgan"
+import { Server } from "socket.io"
+import {createServer} from "node:http"
 const app=express()
+const server=createServer(app)
+const io=new Server(server,{
+    cors:{
+        origin:"*"
+    }
+})
+io.on("connect",(sokcet)=>{
+    console.log("se conecto el usuario",sokcet.id)
+})
 app.use(logger('dev'))
 const port=process.env.PORT||3500
 app.get("/",(req,res)=>{
     res.send("<h1>Helo word</h1>")
 })
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Servidor levantado en http://localhost:${port}/`)
 })
