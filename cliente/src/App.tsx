e30419c2bd6663571989ef01456682056f91fc39
joshua <condorenajoshua98@gmail.com>
2025-10-12 19:38:45 -0500

    Se impleemtno el cliente

---
+++ cliente/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="">
+      <h2 className='text-cyan-300'>hellow world</h2>
+     </div>
+    </>
+  )
+}
+
+export default App
