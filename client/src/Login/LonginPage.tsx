ac43428ba3a4157191423816444983cbf70498b5
joshua <condorenajoshua98@gmail.com>
2025-10-18 20:14:41 -0500

    Se implemento el loginPage

---
+++ client/src/Login/LonginPage.tsx
@@ -0,0 +1,13 @@
+
+import CreateLogin from './Components/CreateLogin'
+
+export default function LonginPage() {
+  return (
+    <>
+   <div className="h-200 overflow-hidden bg-zinc-900 w-400 rounded-2xl ">
+        <h2 className='text-2xl text-cyan-500'>Welcome al login</h2>
+        <CreateLogin></CreateLogin>
+    </div>
+    </>
+  )
+}
