ecbc2c51fb895aa4f4b61a6b768df616924765e0
joshua <condorenajoshua98@gmail.com>
2025-10-20 19:35:46 -0500

    Se implemento el comopnenete regreso de navegacion

---
+++ client/src/hooks/LeftButtonComponent.tsx
@@ -0,0 +1,16 @@
+
+import { AuthUserContext } from "@/UserProvider/UserProvide";
+import { ChevronLeft } from "lucide-react";
+import { useContext } from "react";
+
+export default function LeftButtonComponent() {
+    const {setestadoactual}=useContext(AuthUserContext)
+  return (
+    <>
+        <div onClick={()=>setestadoactual("principal")} 
+        className="hover:bg-zinc-700 duration-300 transition-all p-2 rounded-full ">          
+             <ChevronLeft className="h-5 w-5 text-white"></ChevronLeft>           
+        </div>
+    </>
+  )
+}
