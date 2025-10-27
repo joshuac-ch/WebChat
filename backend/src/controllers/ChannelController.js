eb4b1ad0c557a8d730e6380e1c9b129e83da5ade
joshua <condorenajoshua98@gmail.com>
2025-10-26 20:22:54 -0500

    Se implemento la funcion getll y getID channel

--- backend/src/controllers/ChannelController.js
+++ backend/src/controllers/ChannelController.js
@@ -1,4 +1,5 @@
 import { ChannelModel } from "../models/chanalesModel.js"
+import { UploadCloudnary } from "./UserController.js"
 export const GetChannelID=async(req,res,next)=>{
     try{
         const {id}=req.params
@@ -15,10 +16,27 @@ export const GetChannelID=async(req,res,next)=>{
 export const CreateChannel=async(req,res,next)=>{
     try{
         const {title,description,image,miembros}=req.body
+        let miembrosConvert=JSON.parse(miembros)
+        let imageURL=image
+        if(req.file){
+            imageURL=await UploadCloudnary(req.file.path,"uploads")
+        }
         const channel=await ChannelModel.create({
-            title,description,image,miembros
+            title,description,image:imageURL,miembros:miembrosConvert,tipo:"publico"
         })
         res.status(200).json(channel)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+export const GetAllChannel=async(req,res,next)=>{
+    try{
+        const channels=await ChannelModel.find()
+        if(!channels){
+            return res.status(404).json({message:"No se encontro el modelo"})
+        }
+        res.status(200).json(channels)
     }catch(err){
         console.error(err)
         next(err)
