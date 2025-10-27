158f97998e346cb10542b0ae9ea21f8d1babd680
joshua <condorenajoshua98@gmail.com>
2025-10-26 20:21:14 -0500

    Se modifico el NextComponent

---
+++ client/src/hooks/NextButtonComponent.tsx
@@ -0,0 +1,12 @@
+import { ArrowRight } from 'lucide-react'
+import React from 'react'
+
+export default function NextButtonComponent() {
+  return (
+    <>
+    <div className="mt-4 w-10 h-10 flex items-center justify-center bg-indigo-400 rounded-full p-2">
+        <ArrowRight></ArrowRight>
+    </div>
+    </>
+  )
+}
