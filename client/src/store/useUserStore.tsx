db7e223f5276e3ec757dd6e5f82e9fb5ae610b00
joshua <condorenajoshua98@gmail.com>
2025-10-31 19:05:16 -0500

    Se agreg o un set para manejo de la data del useractualizada enves de un useefect

--- client/src/store/useUserStore.tsx
+++ client/src/store/useUserStore.tsx
@@ -2,12 +2,24 @@ import { axiosIntance } from "@/lib/axios"
 import {create} from "zustand"
 interface UserStore{
     userA:any[]
+    setUserA:(user:string[])=>Promise<void>
     FectUserSpecific:(id)=>Promise<void>
     error:null
     isloading:boolean
+    FectUserAll:()=>Promise<void>
+    alluser:[]
+    miembros:[]
+    setmiembros:(miembros:string[])=>Promise<void>
+    miembrosID:[]
+    setmiembrosID:(miembrosID:string[])=>Promise<void>
 }
 export const useUserStore=create<UserStore>((set)=>({
     userA:[],
+    setUserA:(userA)=>set({userA}),
+    miembrosID:[],
+    setmiembrosID:(miembrosID)=>set({miembrosID}),
+    miembros:[],
+    setmiembros:(miembros)=>set({miembros}),
     error:null,
     isloading:false,
     FectUserSpecific:async(id)=>{
@@ -18,5 +30,15 @@ export const useUserStore=create<UserStore>((set)=>({
         }catch(err:Any){
             set({error:err})
         }  
-    }
+    },
+    alluser:[],
+    FectUserAll:async()=>{
+        set({isloading:true,error:null})
+        try{
+            const {data}=await axiosIntance.get(`/users/g/a`)
+            set({alluser:data})
+        }catch(err:Any){
+            set({error:err})
+        }
+    },
 }))
