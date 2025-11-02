d238fb5682f91bef0d492ea198598ba8dcef09f9
joshua <condorenajoshua98@gmail.com>
2025-11-01 20:24:26 -0500

    Se implemento cambios en la UI

--- client/src/Channel/ChannelPage.tsx
+++ client/src/Channel/ChannelPage.tsx
@@ -1,4 +1,5 @@
 import { useHomeStore } from '@/store/useHomeStore'
+import { useMessageStore } from '@/store/useMessageStore'
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -7,12 +8,13 @@ export default function ChannelPage() {
   useEffect(()=>{
     FecthChannelAll()
   },[FecthChannelAll])
+  const {JoinChannel}=useMessageStore()
   return (
     <>
       <div className="m-4 text-white">
         {channels.map((c,i)=>{
           return(
-            <Link to={`/channel/${c._id}`} className="mt-2 flex hover:bg-white/20 flex-row justify-between items-center rounded-2xl p-2" key={i}>
+            <Link onClick={JoinChannel} to={`/channel/${c._id}`} className="mt-2 flex hover:bg-white/20 flex-row justify-between items-center rounded-2xl p-2" key={i}>
               <div className="flex flex-row gap-4">
                 <div className="">
                   <img src={c.image} className='w-15 h-15 rounded-full' alt="" />
