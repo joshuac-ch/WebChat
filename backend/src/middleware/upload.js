577e63399d29a76048fd67d1e734487b5858a1da
joshua <condorenajoshua98@gmail.com>
2025-11-05 20:03:41 -0500

    Se implemento el middleware para subir images

---
+++ backend/src/middleware/upload.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "../uploads"), // carpeta temporal
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+export default upload;
