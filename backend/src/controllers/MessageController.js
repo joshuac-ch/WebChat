8a55296ff6148dc37f0a7506b12758097529a386
joshua <condorenajoshua98@gmail.com>
2025-11-05 20:03:16 -0500

    Se modifico el create message

---
+++ backend/src/controllers/MessageController.js
@@ -0,0 +1,28 @@
+import { Messagemodel } from "../models/messageModel.js";
+export const GetMessageChannel=async(req,res,next)=>{
+    try{
+        const {channelID}=req.params
+        const messages=await Messagemodel.find({channel:channelID})
+        if(!messages){
+            return res.status(404).json({message:"No se encontraron mensajes"})
+        }
+        res.status(200).json(messages)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+export const CreateMessageChannel=async(req,res,next)=>{
+    try{
+        const {content,channel,User}=req.body
+        const createMessage=await Messagemodel.create({
+            content,
+            channel,
+            User
+        })        
+        res.status(200).json({message:"Se creo el mensaje en el grupo especificado"})
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
