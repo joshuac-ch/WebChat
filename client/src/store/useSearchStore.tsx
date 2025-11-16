0ec6cc4148fd4171f6bcac3edf17aa20859fd93e
joshua <condorenajoshua98@gmail.com>
2025-11-15 20:21:14 -0500

    Se implemento las especificaciones de chat y users

---
+++ client/src/store/useSearchStore.tsx
@@ -0,0 +1,54 @@
+import { axiosIntance } from "@/lib/axios"
+import {create} from "zustand"
+interface UseSearch{
+    FechAllChannel:({search:string})=>Promise<void>;
+    showChannels:any[];
+    isloading:boolean;
+    error:null;
+    ShowUserchat:({user:string})=>Promise<void>;
+    alluserchat:any[];
+    ShowSpecificUser:({user:string})=>Promise<void>
+    datauser:any[]
+}
+
+export const useSearchStore=create<UseSearch>((set)=>({
+    isloading:false,
+    error:null,
+    showChannels:[],
+    alluserchat:[],
+    FechAllChannel:async(search)=>{
+       set({isloading:true,error:null})
+       try{
+        const {data}=await axiosIntance.get(`/channel/g/search/${search}`)
+        set({showChannels:data})
+       }catch(err:any){
+        set({error:err})
+       }finally{
+            set({isloading:false})
+        }     
+    },
+    ShowUserchat:async(user)=>{
+        set({isloading:true,error:null})
+        try{
+            const {data}=await axiosIntance.get(`/users/g/search/${user}`)
+            set({alluserchat:data})
+        }catch(err:any){
+            set({error:err})
+        }finally{
+            set({isloading:false})
+        }
+    },
+    datauser:[],
+    ShowSpecificUser:async(user)=>{
+        set({isloading:true,error:null})
+        try{
+            const {data}=await axiosIntance.get(`/users/${user}`)
+            set({datauser:data})
+        }catch(err:any){
+            set({error:err})
+        }finally{
+            set({isloading:false})
+        }
+    }  
+      
+})) 
