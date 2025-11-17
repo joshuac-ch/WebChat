b22a691d0a95acf964e818bc9f5348301c3c130a
joshua <condorenajoshua98@gmail.com>
2025-11-17 13:44:52 -0500

    Se modifico el main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { BrowserRouter } from 'react-router-dom'
+import UserProvide  from './UserProvider/UserProvide.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+    <UserProvide>
     <App />
+    </UserProvide>    
+    </BrowserRouter>
   </StrictMode>,
 )
