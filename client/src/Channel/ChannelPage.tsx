f1a11e1e246127c651d49d50cc071cad704a9f98
joshua <condorenajoshua98@gmail.com>
2025-11-17 13:45:21 -0500

    Se modifico el channel page en UI y channels

--- client/src/Channel/ChannelPage.tsx
+++ client/src/Channel/ChannelPage.tsx
@@ -1,23 +1,30 @@
+import DeleteChat from '@/components/DeleteChat'
 import { useHomeStore } from '@/store/useHomeStore'
 import { useMessageStore } from '@/store/useMessageStore'
+import { useUserStore } from '@/store/useUserStore'
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function ChannelPage() {
-  const {channels,FecthChannelAll}=useHomeStore()
+  const {channelUser,AllchannelUser}=useHomeStore()
+  const {userA}=useUserStore()
   useEffect(()=>{
-    FecthChannelAll()
-  },[FecthChannelAll])
-  const {JoinChannel}=useMessageStore()
+    AllchannelUser(userA._id)
+  },[AllchannelUser,userA])
+  const {JoinChannel,JoinChat}=useMessageStore()
   return (
     <>
       <div className="m-4 text-white">
-        {channels.map((c,i)=>{
+        {channelUser.map((c,i)=>{
+          const canales=c.tipo=="publico"
+          const chatsusers=c.tipo=="privado"
           return(
-            <Link onClick={()=>JoinChannel({canalID:c._id})} to={`/channel/${c._id}`} className="mt-2 flex hover:bg-white/20 flex-row justify-between items-center rounded-2xl p-2" key={i}>
+           <div className="">
+             {canales&&(
+              <Link onClick={()=>JoinChannel({canalID:c._id})} to={`/channel/${c._id}`} className="mt-2 flex hover:bg-white/20 flex-row justify-between items-center rounded-2xl p-2" key={i}>
               <div className="flex flex-row gap-4">
                 <div className="">
-                  <img src={c.image} className='w-15 h-15 rounded-full' alt="" />
+                  <img src={c.image} className='w-15 h-15 rounded-full object-cover' alt="" />
                 </div>
                 <div className="text-start">
                   <p>{c.title}</p>
@@ -25,9 +32,28 @@ export default function ChannelPage() {
                 </div>
               </div>
               <div className="">
+                <DeleteChat id={c._id}></DeleteChat>
                 <p>{c.updatedAt?c.updatedAt.split("T")[0]:""}</p>
               </div>
             </Link>
+            )}
+            {chatsusers&&(
+              <Link onClick={()=>JoinChat({chatID:c.miembros[1]})} to={`/chatuser/${c.miembros[1]}`} className="mt-2 flex hover:bg-white/20 flex-row justify-between items-center rounded-2xl p-2" key={i}>
+              <div className="flex flex-row gap-4">
+                <div className="">
+                  <img src={c.image}  className='w-15 h-15 rounded-full object-cover' alt="" />
+                </div>
+                <div className="text-start">
+                  <p>{c.title}</p>
+                  <p>{c.description}</p>
+                </div>
+              </div>
+              <div className="">
+                <DeleteChat id={c._id}></DeleteChat>
+                <p>{c.updatedAt?c.updatedAt.split("T")[0]:""}</p>
+              </div>
+            </Link>)}            
+           </div>
           )
         })}
       </div>
