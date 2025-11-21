73e5c6d7e5bcb9e1d5b009ade35c53ee31076973
joshua <condorenajoshua98@gmail.com>
2025-11-21 13:30:58 -0500

    Se modifico el canal

--- client/src/Buscador/Components/Channels.tsx
+++ client/src/Buscador/Components/Channels.tsx
@@ -1,5 +1,7 @@
+import { useMessageStore } from "@/store/useMessageStore"
 import { useSearchStore } from "@/store/useSearchStore"
 import { useEffect } from "react"
+import { Link } from "react-router-dom"
 
 
 export default function Channels({search}) {
@@ -7,21 +9,25 @@ export default function Channels({search}) {
   useEffect(()=>{
     FechAllChannel(search)
     },[FechAllChannel,search])
+  const {JoinChannel}=useMessageStore()  
   return (
     <>
     <div className="m-4">
         <p>Bienvenido a canales</p>
         {showChannels.map((s,i)=>{
+          const cate=s.categoria=="canal"
           return(
-            <div className="p-2 mt-4 flex flex-row items-center gap-4 hover:bg-zinc-700 rounded-md" key={i}>
-             <div className="">
-              <img src={s.image} className="w-15 h-15 rounded-full" alt="" />
-             </div>
-             <div className="text-start">
-               <p>{s.title}</p>
-              <p>{s.description}</p>
-             </div>
-            </div>
+            cate&&(
+              <Link to={`/channel/${s._id}`} onClick={()=>JoinChannel({canalID:s._id})} className="p-2 mt-4 flex flex-row items-center gap-4 hover:bg-zinc-700 rounded-md" key={i}>            
+              <div className="">
+               <img src={s.image} className="w-15 h-15 rounded-full" alt="" />
+              </div>
+              <div className="text-start">
+                <p>{s.title}</p>
+               <p>{s.description}</p>
+              </div>            
+            </Link>
+            )
           )
         })}
     </div>
