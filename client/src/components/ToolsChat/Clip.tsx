80963774321f9c7aac3dfcca122832bd6a942966
joshua <condorenajoshua98@gmail.com>
2025-11-21 13:29:33 -0500

    Se modifioc las herramientas de chat

--- client/src/components/ToolsChat/Clip.tsx
+++ client/src/components/ToolsChat/Clip.tsx
@@ -1,44 +1,48 @@
 import { File, Image, NotebookText, Paperclip } from "lucide-react";
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger } from "../ui/menubar";
-import { useRef, useState } from "react";
+import { useRef} from "react";
 import { Button } from "../ui/button";
 
+interface ClipProps{
+  onPreview:(url:string|null)=>void;
+  OnState:(estado:boolean|null)=>void
 
-export default function Clip() {
-  const [searchFile, setsearchFile] = useState<File|null>()
-  const fileref= useRef<HTMLInputElement>(null)
-  const [previwe, setpreviwe] = useState<string|null>(null)
-  const OpenFile=(e:File)=>{    
-    if(e){
-       setpreviwe(URL.createObjectURL(e))
-       setsearchFile(e)
-       console.log(searchFile)
-    }
+}
+export default function Clip({onPreview,OnState}:ClipProps) {
+  
+  const fileref= useRef<HTMLInputElement>(null)  
+  const OpenFile=(file:File)=>{       
+       if (!file) return;
+          const url = URL.createObjectURL(file)
+          onPreview(url)
+          OnState(true)
+          // Muy importante:
+          // Resetea el input para poder seleccionar la MISMA imagen otra vez.
+          if (fileref.current) fileref.current.value = ""
   }  
   return (
     <>
+    <input type="file" accept="image/*" name="" className="hidden" onChange={(e)=>OpenFile(e.target.files?.[0])} ref={fileref}  id="" />
     <Menubar>
         <MenubarMenu>
             <MenubarTrigger><Paperclip className="w-6 h-6"></Paperclip></MenubarTrigger>
             <MenubarContent>
                 <MenubarItem >
-                    <Button onClick={()=>fileref.current?.click()}></Button>
-                   
-                   <Image></Image> Photo
+                    <Button onClick={()=>fileref.current?.click()}><Image></Image></Button>                    
+                    Photo
                 </MenubarItem>
                 <MenubarSeparator />
                 <MenubarItem><File></File> File
-                 <input type="file" accept="image/*" name="" className="hidden" onChange={(e)=>OpenFile(e.target.files?.[0])} ref={fileref}  id="" />
+               
                 </MenubarItem>
                 <MenubarSeparator />
                 <MenubarItem><NotebookText></NotebookText> Encuesta
-                {previwe&&(
-                    <img src={previwe} className="w-10 h-10" alt="" />
-                )}
+                
                 </MenubarItem>            
             </MenubarContent>
         </MenubarMenu>
     </Menubar>
+    
     </>
   )
 }
