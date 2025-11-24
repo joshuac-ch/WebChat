2e64fbc414513a37c83847387631a32d6edd0112
joshua <condorenajoshua98@gmail.com>
2025-11-24 13:42:34 -0500

    Se modifico el SearchComponent en la UI y Fronted

--- client/src/components/SearchComponent.tsx
+++ client/src/components/SearchComponent.tsx
@@ -1,11 +1,15 @@
 import { useUserStore } from "@/store/useUserStore"
 
-import {  Paperclip, Send, Smile } from "lucide-react"
+import {  Send, Smile } from "lucide-react"
 import { useEffect, useState } from "react"
 import { Input } from "./ui/input"
-import { toast, Toaster } from "sonner"
-import { axiosIntance } from "@/lib/axios"
+import { Toaster } from "sonner"
+
 import { useMessageStore } from "@/store/useMessageStore"
+import Clip from "./ToolsChat/Clip"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
+import { Button } from "./ui/button"
+import SendMessageFile from "./SendMessageFile"
 
 export default function SearchComponent({channelID,userID}) {
   const {userA,FectUserSpecific}=useUserStore()   
@@ -13,40 +17,46 @@ export default function SearchComponent({channelID,userID}) {
   useEffect(()=>{
     FectUserSpecific(userA?._id)
   },[FectUserSpecific,userA?._id]) 
+  const {currentchannel}=useMessageStore()
+  const [content, setcontent] = useState("")
   const [DataFormMessage, setDataFormMessage] = useState({
-    content:"",
-    User:userID,
-    channel:channelID
+    content:""
+    //User:"",
+    //channel:""
   })
-  const CreteMessage=async()=>{
-    console.log(DataFormMessage)
-    try{
-        if(!DataFormMessage.content){
-            toast.error("No se envio ningun mensaje")
-        }
-        await axiosIntance.post("/message/c/messagechannel",DataFormMessage)
-        toast.message("Se envio el mensaje")
-        setDataFormMessage((prev)=>({
-            ...prev,
-            content:""        
-    }))
-    }catch(err){
-        console.error(err)
-        
-    }
-  }
+  
   const {SendMessage}=useMessageStore()
+  const [previewImg, setpreviewImg] = useState<File|null>(null)
+  const [estadoDialog, setestadoDialog] = useState(false)
+  const [imageFile, setimageFile] = useState<File|null>(null)
   return (
    <>
    <Toaster></Toaster>
+   <Dialog open={estadoDialog} onOpenChange={setestadoDialog}>    
+    <DialogContent className="bg-zinc-900 text-white">
+        <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>            
+        </DialogHeader>
+        {previewImg&&(
+                    <img src={previewImg} className="w-full h-full rounded-md" alt="" />
+                    
+                )}
+            <SendMessageFile img={imageFile}></SendMessageFile>
+        <DialogFooter className="text-black">
+            <Button onClick={()=>{
+                setpreviewImg(null)
+                setestadoDialog(false)}}  variant="outline">Cancel</Button>      
+        </DialogFooter>          
+    </DialogContent>
+     
+    </Dialog>
    <div className="flex flex-col">
         <div className="border-white/20 border-t-1 w-full mb-2 "></div>
         <div className="flex flex-row items-center mb-4 gap-2">  
             <div className="bg-zinc-800 p-1 rounded-2xl w-180 flex flex-row items-center justify-between">    
                 <div className="flex flex-row items-center gap-2 ">
                     <div className="">
-                        <img src={userA?.profile} className="w-12 h-12 object-cover rounded-full" alt="" />
-                        
+                        <img src={userA?.profile} className="w-12 h-12 object-cover rounded-full" alt="" />                        
                     </div>
                     <div className="">
                         <Smile className="w-8 h-8"></Smile>
@@ -56,9 +66,11 @@ export default function SearchComponent({channelID,userID}) {
                 <div className="w-full max-w-140 ">
                         <Input value={DataFormMessage.content} onChange={(e)=>setDataFormMessage({...DataFormMessage,content:e.target.value})} placeholder="Message"></Input>
                     </div>
+
                 <div className="">
-                    <Paperclip></Paperclip>
+                    <Clip onPreview={setpreviewImg} OnFile={setimageFile} OnState={setestadoDialog}></Clip>
                 </div>
+                
             </div>
             <div onClick={()=>SendMessage(DataFormMessage)} className="bg-indigo-500 p-2 rounded-full">         
                     <Send className="w-8 h-8"></Send>        
