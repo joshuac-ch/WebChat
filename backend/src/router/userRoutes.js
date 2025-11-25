1769d7de9b0b1a3a9ff1fd35d204237a2870245b
joshua <condorenajoshua98@gmail.com>
2025-11-25 13:37:12 -0500

    Se modifico el userRoutes

--- backend/src/router/userRoutes.js
+++ backend/src/router/userRoutes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { CreateUsuariosID, EditUser, GetAllUser, Login, UserSpecific } from "../controllers/UserController.js";
+import { CreateChatUser, CreateUsuariosID, EditUser, GetAllUser,  Login, SearchUserChat, UserSpecific } from "../controllers/UserController.js";
 import upload from "../middleware/upload.js";
 
 const router=Router()
 router.post("/c",CreateUsuariosID)
 router.get("/:id",UserSpecific)
 router.get("/g/a",GetAllUser)
+router.get("/g/search/:user",SearchUserChat)
 router.post("/v",Login)
 router.put("/e/u/:id",upload.single('imageFile'),EditUser)
+router.post("/chat/private/:usera/:userb",CreateChatUser)
+
 export default router
