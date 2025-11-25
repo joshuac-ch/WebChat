2da28831cdacadd6d97d6a49375a768affdd4082
joshua <condorenajoshua98@gmail.com>
2025-11-25 13:36:50 -0500

    Se modifoc el SendMessageFile

--- backend/src/router/MessageRoutes.js
+++ backend/src/router/MessageRoutes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { CreateMessageChannel, GetMessageChannel } from "../controllers/MessageController.js";
+import { CreateMessageChannel, CreateMessageFile, GetMessageChannel } from "../controllers/MessageController.js";
+import upload from "../middleware/upload.js";
 const router=Router()
 router.get("/g/messagechannel/:channelID",GetMessageChannel)
 router.post("/c/messagechannel",CreateMessageChannel)
+
+router.post("/c/messageFile",upload.single("imageURL"),CreateMessageFile)
 export default router
