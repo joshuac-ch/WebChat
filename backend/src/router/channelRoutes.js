08b5e88671216a5f17fde266a1e5a0aa0f57df8d
joshua <condorenajoshua98@gmail.com>
2025-11-25 13:37:36 -0500

    Se agregaron muevas rutas para el channel

--- backend/src/router/channelRoutes.js
+++ backend/src/router/channelRoutes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { CreateChannel, GetChannelID } from "../controllers/ChannelController.js";
+import { CreateChannel, DeleteChatUSER, GetAllChannel, GetChannelCategoria, GetChannelID, GetChannelUser, SearchChannel } from "../controllers/ChannelController.js";
+import upload from "../middleware/upload.js";
 
 const router=Router()
-router.post("/c",CreateChannel)
+router.get("/g/search/:query",SearchChannel)
+router.post("/c",upload.single("imageFile"),CreateChannel)
+router.get("/g/a",GetAllChannel) //forma global
 router.get("/g/:id",GetChannelID)
+router.get("/u/a/:id",GetChannelUser)
+router.get("/chat-users/:id/",GetChannelCategoria)
+router.delete("/chat-delete/:id",DeleteChatUSER)
 export default router
