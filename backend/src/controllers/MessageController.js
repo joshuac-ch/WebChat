1db91ac6de2ca2f32182851683a3249c1e58a47b
joshua <condorenajoshua98@gmail.com>
2025-12-09 14:30:39 -0500

    Se modifico el controller de message

--- backend/src/controllers/MessageController.js
+++ backend/src/controllers/MessageController.js
@@ -1,4 +1,5 @@
 import { Messagemodel } from "../models/messageModel.js";
+import { UploadCloudnary } from "./UserController.js";
 export const GetMessageChannel=async(req,res,next)=>{
     try{
         const {channelID}=req.params
@@ -7,6 +8,27 @@ export const GetMessageChannel=async(req,res,next)=>{
             return res.status(404).json({message:"No se encontraron mensajes"})
         }
         res.status(200).json(messages)
+    }catch(err){
+        console.error(err)
+        next(err)
+    }
+}
+export const CreateMessageFile=async(req,res,next)=>{
+    try{
+        const {imageURL,type,content,channel,User}=req.body
+        let imageFile=imageURL
+        if(req.file){
+            imageFile=await UploadCloudnary(req.file.path)
+        }      
+
+        const createMessage=await Messagemodel.create({
+            imageURL:imageFile,
+            type:"mix",
+            content,
+            channel,
+            User
+        })
+        res.status(200).json(createMessage)
     }catch(err){
         console.error(err)
         next(err)
