80e7257ff162bbadd56ee6d357df47afd29967ac
joshua <condorenajoshua98@gmail.com>
2025-12-10 13:05:49 -0500

    Se modifico el chatspeciifico

--- client/src/Buscador/Components/chats/ChatSpecific.tsx
+++ client/src/Buscador/Components/chats/ChatSpecific.tsx
@@ -1,4 +1,8 @@
+import HearderChat from "@/components/HearderChat"
 import PlantillaChats from "@/components/PlantillaChats"
+import SearchChat from "@/components/SearchChat"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { useHomeStore } from "@/store/useHomeStore"
 import { useMessageStore } from "@/store/useMessageStore"
 import { useSearchStore } from "@/store/useSearchStore"
 import { useEffect, useState } from "react"
@@ -7,8 +11,11 @@ import { useParams } from "react-router-dom"
 export default function ChatSpecific() {
   const {userid}=useParams()
   const {ShowSpecificUser,datauser}=useSearchStore()  
+  const {FecthChannelAll,channels}=useHomeStore()
+  console.log(channels)
   useEffect(()=>{
     ShowSpecificUser(userid)
+    //FecthChannelAll()
   },[userid])
   const [user, setuser] = useState({
     create:datauser.createdAt,
@@ -20,9 +27,21 @@ export default function ChatSpecific() {
     }
   },[datauser])
   const {messageChat}=useMessageStore()
+  
+  
   return (
     <>
-     <PlantillaChats mensajes={messageChat} datachat={datauser} fecha_create={user.create} foto={user.image} id={userid}></PlantillaChats>
+     <div className="text-white flex flex-col h-full justify-start w-full">
+        <div className="">
+            <HearderChat data={datauser}></HearderChat>
+        </div>
+        <ScrollArea className="h-full">
+         <PlantillaChats mensajes={messageChat} datachat={datauser} fecha_create={user.create} foto={user.image} id={userid}></PlantillaChats>
+        </ScrollArea>
+     </div>
+     <div className="text-white flex flex-row justify-center">
+        <SearchChat></SearchChat>         
+     </div>
     </>
   )
 }
