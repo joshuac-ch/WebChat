3d46af117d93e3cef18c7e149811d38df2b7fd6a
joshua <condorenajoshua98@gmail.com>
2025-12-12 12:56:05 -0500

    Se modifico la UI de el login

--- client/src/Login/LonginPage.tsx
+++ client/src/Login/LonginPage.tsx
@@ -1,11 +1,20 @@
 
+import { Button } from '@/components/ui/button'
 import CreateLogin from './Components/CreateLogin'
+import { Link } from 'react-router-dom'
 
 export default function LonginPage() {
   return (
     <>
    <div className="h-200 overflow-hidden bg-zinc-900 w-400 rounded-2xl ">
-        <h2 className='text-2xl text-cyan-500'>Welcome al login</h2>
+        <div className="text-white flex flex-row justify-around items-center pt-4 gap-10">
+          <Link to={"/sing"}>
+          <Button variant={'ghost'} className='cursor-pointer'>Regresar</Button>
+          </Link>
+         <h2 className='text-2xl text-cyan-500'>Welcome al login</h2>
+         <h1 ></h1>
+        </div>
+         
         <CreateLogin></CreateLogin>
     </div>
     </>
