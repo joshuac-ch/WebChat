61ee84109fffc757d76ada618a17067066f2d05f
joshua <condorenajoshua98@gmail.com>
2025-12-14 12:59:57 -0500

    Se modifico el main

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -20,6 +20,7 @@ export default function MainLayout() {
                      
                     </div>                
             </ResizablePanel>
+            
         </ResizablePanelGroup>
     </div>
     </>
