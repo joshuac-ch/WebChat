10350bcd337893ec2c7103f8744aaef878d8b951
joshua <condorenajoshua98@gmail.com>
2025-12-14 13:00:54 -0500

    Se muesta los datos de manera correcta usando la ley de envio de notificacioes de promps

--- client/src/Channel/Components/ChannelSpecific.tsx
+++ client/src/Channel/Components/ChannelSpecific.tsx
@@ -7,6 +7,8 @@ import SearchComponent from '@/components/SearchComponent'
 import { useUserStore } from '@/store/useUserStore'
 import { useMessageStore } from '@/store/useMessageStore'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { ArrowUpRight } from 'lucide-react'
+import RightLayout from '@/layout/components/RightLayout'
 
 export default function ChannelSpecific() {
     const {id}=useParams()
@@ -27,13 +29,17 @@ export default function ChannelSpecific() {
       }
     },[channelSpecific])
     const {messagesChannel}=useMessageStore()
+    const [showstate, setshowstate] = useState(false)
+    
   return (
     <>
     <div className="text-white flex flex-col h-200 justify-start w-full">
        <div className="">
-            <HeaderChannel data={channelSpecific}></HeaderChannel>
+            <HeaderChannel data={channelSpecific} setrigthstate={setshowstate}></HeaderChannel>
        </div>
-       <div className="h-165">
+      <div className={`${showstate?"grid grid-cols-2":""}`}>
+        <div className="">
+          <div className="h-165">
        <ScrollArea className='h-full'>
         <div className="h-full flex flex-col justify-start">
           <div className="mt-4">
@@ -42,29 +48,69 @@ export default function ChannelSpecific() {
           </div>
           <div className="mt-4 flex flex-col items-center">
             <p>Channel Photon Updated</p>
-            <img src={DataChannel.foto} className='mt-2 w-40 h-40 rounded-full' alt="" />
+            <img src={DataChannel.foto} className='mt-2 w-50 h-50 object-cover rounded-full' alt="" />
           </div>
           <div className="flex flex-col gap-3 px-12 p-4 items-start">
-              {messagesChannel.length>0?
-              messagesChannel.map((m,i)=>{
+            {messagesChannel.length>0?
+               messagesChannel.map((m,i)=>{
+                const texto=m.type=="text"
+                const mix=m.type=="mix"
                 return(
-                  <div key={i} className='p-2 rounded-md w-40 bg-white/20'>
+                  <div className="" key={i}>
+             
+                  {texto&&(
+                    <div key={i} className="relative flex flex-row items-end group">
+                    <div  className='relative p-2 rounded-md max-w-100 min-w-25 min-h-15  bg-zinc-800'>
                     <p className='text-start'>{m.content}</p>
                     <div className="">
-                      <p className='text-end text-[13px]'>{new Date(m.createdAt).getFullYear()}</p>
+                      <p className='text-end text-[13px] text-gray-500'>{new Date(m.createdAt).getFullYear()}</p>
+                    </div>
+                    
+                  </div>
+                  <div className="cursor-pointer ml-2 border-2  border-white/20 bg-zinc-800 rounded-md hidden group-hover:block transition-all duration-300 ">
+                      <ArrowUpRight />
                     </div>
                   </div>
-                )
-              })
-              :
-              "no hay data"}
+                  )}
+                 
+               
+                {mix&&(
+                  <div key={i} className="relative flex flex-row items-end group">
+                    <div  className='relative p-2 rounded-md max-w-100 min-w-25 min-h-15  bg-zinc-800'>
+                    <img src={m.imageURL} className='w-full h-full object-cover rounded-md' alt="" />
+                    <p className='text-start'>{m.content}</p>
+                    <div className="">
+                      <p className='text-end text-[13px] text-gray-500'>{new Date(m.createdAt).getFullYear()}</p>
+                    </div>
+                    
+                  </div>
+                  <div className="cursor-pointer ml-2 border-2  border-white/20 bg-zinc-800 rounded-md hidden group-hover:block transition-all duration-300 ">
+                      <ArrowUpRight />
+                    </div>
+                  </div>
+                )}
+                   
+                  </div>
+                )   
+              
+            })      
+             
+            :
+            "cargando..."}
           </div>
         </div>
        </ScrollArea>
-       </div>
-       <div className="flex flex-row justify-center">
-          <SearchComponent userID={userA?._id} channelID={id}></SearchComponent>
-       </div>
+          </div>
+          <div className="flex flex-row justify-center">
+            <SearchComponent userID={userA?._id} channelID={id}></SearchComponent>
+          </div>
+        </div>
+        <div className="">
+          {showstate&&(
+            <RightLayout data={channelSpecific} Onclose={setshowstate}></RightLayout>
+          )}
+        </div>
+      </div>
     </div>
     </>
   )
