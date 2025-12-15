9b6a2613493e911aa585ff244fa50231fbc5c51b
joshua <condorenajoshua98@gmail.com>
2025-12-15 12:33:30 -0500

    Se modifico el router user

--- backend/src/router/userRoutes.js
+++ backend/src/router/userRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { CreateChatUser, CreateUsuariosID, EditUser, GetAllUser,  Login, SearchUserChat, UserSpecific } from "../controllers/UserController.js";
+import { CreateChatUser, CreateUsuariosID, EditUser, GetAllUser,  Login, SearchSpecificChat, SearchUserChat,SearchChatTest, UserSpecific } from "../controllers/UserController.js";
 import upload from "../middleware/upload.js";
 
 const router=Router()
 router.post("/c",CreateUsuariosID)
 router.get("/:id",UserSpecific)
+//para la busqueda del usuario
+router.get("/search/:id",SearchChatTest)
+//conection entre 2 chats para mostrar su informacion
+router.get("/specific-chat/:id",SearchSpecificChat)
 router.get("/g/a",GetAllUser)
 router.get("/g/search/:user",SearchUserChat)
 router.post("/v",Login)
