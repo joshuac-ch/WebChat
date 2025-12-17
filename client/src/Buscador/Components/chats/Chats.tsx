73f0135251f8c3fb4f9561bb32bcb667c4e835d7
joshua <condorenajoshua98@gmail.com>
2025-12-17 13:31:41 -0500

    el page chat se mejoro para busqueda

--- client/src/Buscador/Components/chats/Chats.tsx
+++ client/src/Buscador/Components/chats/Chats.tsx
@@ -1,5 +1,7 @@
+import { useHomeStore } from "@/store/useHomeStore";
 import { useMessageStore } from "@/store/useMessageStore";
 import { useSearchStore } from "@/store/useSearchStore"
+import { useUserStore } from "@/store/useUserStore";
 import { useEffect } from "react"
 import { Link } from "react-router-dom";
 interface ChatsProps {
@@ -7,12 +9,14 @@ interface ChatsProps {
 }
 export default function Chats({search}:ChatsProps) {
   const {ShowUserchat,alluserchat} =useSearchStore()
+  const {userA}=useUserStore()
   const {JoinChat}=useMessageStore()
   useEffect(()=>{
    if(search?.trim()){
      ShowUserchat(search)
    }
   },[search])
+  
   return (
     <>
     <div className="">
@@ -21,13 +25,14 @@ export default function Chats({search}:ChatsProps) {
     <div className="">
       {alluserchat.map((u,i)=>{
         return(
-          <Link onClick={()=>JoinChat({chatID:u._id})} to={`/chatuser/${u._id}`} className="flex flex-row  items-center gap-4 hover:bg-zinc-700 p-2 mt-4" key={i}>
+          <Link onClick={()=>JoinChat({chatID:u._id})} to={`/search/${u._id}`} className="flex flex-row  items-center gap-4 hover:bg-zinc-700 p-2 mt-4" key={i}>
          
             <div className="">
               <img src={u.profile} className="w-15 h-15 rounded-full object-cover" alt="" />
             </div>
             <div className="">
               <p>{u.name}</p>
+              <p>{u._id}</p>
              
             </div>
          
