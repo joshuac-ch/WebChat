e85abc44f6972e65d444cbb6c1c95e602d432bfe
joshua <condorenajoshua98@gmail.com>
2025-12-17 13:31:08 -0500

    Se mejoro la funcion del suauri

--- client/src/Buscador/Components/chats/ChatSpecific.tsx
+++ client/src/Buscador/Components/chats/ChatSpecific.tsx
@@ -9,33 +9,37 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function ChatSpecific() {
-  const {userid}=useParams()
-  const {ShowSpecificUser,datauser}=useSearchStore()  
+  const {userid,id}=useParams()
+  const {ShowSpecificUser,ShowConnectAnotherUser,connectuser,datauser}=useSearchStore()  
   const {FecthChannelAll,channels}=useHomeStore()
   
   useEffect(()=>{
-    ShowSpecificUser(userid)
-    //FecthChannelAll()
-  },[userid])
-  const [user, setuser] = useState({
-    create:datauser.createdAt,
-    image:datauser.profile,
-  })
-  useEffect(()=>{
-    if(datauser){
-        setuser({create:datauser.createdAt,image:datauser.profile})
+    //ShowSpecificUser(userid)
+    if(id && userid){
+      ShowConnectAnotherUser(id,userid)
     }
-  },[datauser])
+    //FecthChannelAll()
+  },[id,userid])
+  const [user, setuser] = useState({
+    create:connectuser.createdAt,
+    image:connectuser.profile,
+  })
+ 
+  useEffect(()=>{
+    if(connectuser){
+        setuser({create:connectuser.createdAt,image:connectuser.profile})
+    }
+  },[connectuser])
   const {messageChat}=useMessageStore()  
   
   return (
     <>
      <div className="text-white flex flex-col h-full justify-start w-full">
         <div className="">         
-            <HearderChat data={datauser}></HearderChat>
+            <HearderChat data={connectuser}></HearderChat>
         </div>
         <ScrollArea className="h-full">
-         <PlantillaChats mensajes={messageChat} datachat={datauser} fecha_create={user.create} foto={user.image} id={userid}></PlantillaChats>
+         <PlantillaChats mensajes={messageChat} datachat={connectuser} fecha_create={user.create} foto={user.image} id={id}></PlantillaChats>
         </ScrollArea>
         
      </div>
