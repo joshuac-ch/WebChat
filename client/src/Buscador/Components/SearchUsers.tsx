764d1617dae7dcbd0a09080abdaa71e279df6c6a
joshua <condorenajoshua98@gmail.com>
2025-12-22 22:53:35 -0500

    Se implenemnro el search user y se modifico la ui

---
+++ client/src/Buscador/Components/SearchUsers.tsx
@@ -0,0 +1,47 @@
+import HearderChat from "@/components/HearderChat";
+import PlantillaChats from "@/components/PlantillaChats";
+import SearchChat from "@/components/SearchChat";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useHomeStore } from "@/store/useHomeStore";
+import { useMessageStore } from "@/store/useMessageStore";
+import { useSearchStore } from "@/store/useSearchStore";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function SearchUsers() {
+    const {id}=useParams()
+    const {ShowSpecificUser,datauser}=useSearchStore()  
+    const {FecthChannelAll,channels}=useHomeStore()
+   
+   useEffect(()=>{
+     ShowSpecificUser(id)
+     //ShowConnectAnotherUser(id)
+     //FecthChannelAll()
+   },[id])
+   const [user, setuser] = useState({
+     create:datauser.createdAt,
+     image:datauser.profile,
+   })
+   useEffect(()=>{
+     if(datauser){
+         setuser({create:datauser.createdAt,image:datauser.profile})
+     }
+   },[datauser])
+   const {messageChat}=useMessageStore()  
+  return (
+    <>
+    <div className="text-white flex flex-col h-full justify-start w-full">
+        <div className="">         
+            <HearderChat data={datauser}></HearderChat>
+        </div>
+        <ScrollArea className="h-full">
+         <PlantillaChats mensajes={messageChat} datachat={datauser} fecha_create={user.create} foto={user.image} id={id}></PlantillaChats>
+        </ScrollArea>
+        
+     </div>
+     <div className="text-white flex flex-row justify-center">
+        <SearchChat></SearchChat>         
+     </div>
+    </>
+  )
+}
