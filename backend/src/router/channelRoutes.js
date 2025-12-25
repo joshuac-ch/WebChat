f317a18bbd17c89bf30be793b8fe9daae2ca65fd
joshua <condorenajoshua98@gmail.com>
2025-12-24 19:12:18 -0500

    Se implemento nuevas rutas para los canales

--- backend/src/router/channelRoutes.js
+++ backend/src/router/channelRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { CreateChannel, DeleteChatUSER, GetAllChannel, GetChannelCategoria, GetChannelID, GetChannelUser, SearchChannel } from "../controllers/ChannelController.js";
+import { CreateChannel, DeleteChatUSER, GetAllChannel, GetChannelCategoria, GetChannelID, GetChannelUser, SearchChannel, UpdateChannel } from "../controllers/ChannelController.js";
 import upload from "../middleware/upload.js";
 
 const router=Router()
 router.get("/g/search/:query",SearchChannel)
 router.post("/c",upload.single("imageFile"),CreateChannel)
 router.get("/g/a",GetAllChannel) //forma global
-router.get("/g/:id",GetChannelID)
+router.get("/g/:id",GetChannelID)//mostrar canal
 router.get("/u/a/:id",GetChannelUser)
 router.get("/chat-users/:id/",GetChannelCategoria)
 router.delete("/chat-delete/:id",DeleteChatUSER)
+router.put("/update/:id",upload.single("image"),UpdateChannel)
 export default router
